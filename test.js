681430cc664779bc0989c75da42161bc6bbd2114
Kenrick <kenrick95@gmail.com>
2018-11-06 10:40:23 +0800

    It works!

--- test.js
+++ test.js
@@ -1,38 +1,27 @@
+// Note: getBoardScale reads from "window.innerWidth"
+global.window = {
+  innerWidth: 1024
+};
 var PImage = require('pureimage');
 var canvas = PImage.make(640, 480).getContext('2d');
 var c4game = require('@kenrick95/c4');
 
-const { GameBase, PlayerAi, Player, BoardPiece } = c4game;
+const { GameBase, PlayerAi, PlayerHuman, BoardPiece, BoardBase } = c4game;
 
 class Game extends GameBase {
-  constructor(players, canvas) {
-    super(players, canvas)
+  constructor(players, board) {
+    super(players, board);
+  }
+  afterMove() {
+    // no-op
   }
 }
 
-class PlayerHuman extends Player {
-  constructor(boardPiece, canvas) {
-    super(boardPiece, canvas)
-    this.clickPromiseResolver = null
-  }
-
-  doAction(column) {
-    if (this.clickPromiseResolver && 0 <= column && column < Board.COLUMNS) {
-      this.clickPromiseResolver(column)
-    }
-  }
-
-  async getAction(board) {
-    return new Promise(r => (this.clickPromiseResolver = r))
-  }
-}
-
-
+const board = new BoardBase(canvas);
+const playerHuman = new PlayerHuman(BoardPiece.PLAYER_1);
 const game = new Game(
-  [
-    new PlayerHuman(BoardPiece.PLAYER_1, canvas),
-    new PlayerAi(BoardPiece.PLAYER_2, canvas),
-  ],
-  canvas
+  [playerHuman, new PlayerAi(BoardPiece.PLAYER_2)],
+  board
 );
 game.start();
+playerHuman.doAction(4);
